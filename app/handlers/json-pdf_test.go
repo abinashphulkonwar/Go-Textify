89c4fd79177bfd271ae32e6f3ad65487783625c8
abinash <abinashphulkonwar1@gmail.com>
2023-09-09 10:54:06 +0530

    add json to pdf test

---
+++ app/handlers/json-pdf_test.go
@@ -0,0 +1,13 @@
+package handlers_test
+
+import (
+	"testing"
+
+	"github.com/abinashphulkonwar/Textify/app/handlers"
+)
+
+func TestJsonToPdf(t *testing.T) {
+
+	handlers.JsonToPdf("fac6910610ae0233ab595005d6144e2d.json")
+
+}
