e9f26be3fd0608132bd3f07aa6a3843f33fe043a
abinash <abinashphulkonwar1@gmail.com>
2023-09-14 19:35:09 +0530

    add file type check handler io.Reader read issue fixed

--- app/handlers/file-type-check.go
+++ app/handlers/file-type-check.go
@@ -9,7 +9,11 @@ import (
 	errorhandlers "github.com/abinashphulkonwar/Textify/error-handler"
 )
 
-func CheckFileTypeHandler(file *os.File) {
+func CheckFileTypeHandler(path string) {
+	file, err := os.Open(path)
+	errorhandlers.HandleError(err)
+	defer file.Close()
+
 	stat, err := file.Stat()
 	errorhandlers.HandleError(err)
 	if stat.IsDir() {
@@ -18,9 +22,11 @@ func CheckFileTypeHandler(file *os.File) {
 	buf := make([]byte, 1024)
 
 	_, err = file.Read(buf)
+
 	errorhandlers.HandleError(err)
 
 	contentType := http.DetectContentType(buf)
+
 	println("file type: ", contentType)
 	if !strings.Contains(contentType, "image/") {
 		errorhandlers.HandleError(errors.New("not a valid file"))
--- app/handlers/file-type-check_test.go
+++ app/handlers/file-type-check_test.go
@@ -1,20 +1,13 @@
 package handlers_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/abinashphulkonwar/Textify/app/handlers"
-	errorhandlers "github.com/abinashphulkonwar/Textify/error-handler"
 )
 
 func TestFileTypeCheck(t *testing.T) {
 
-	file, err := os.Open("../../20230816_133449.jpg")
-	errorhandlers.HandleError(err)
-
-	defer file.Close()
-
-	handlers.CheckFileTypeHandler(file)
+	handlers.CheckFileTypeHandler("../../20230816_133449.jpg")
 
 }
